0a56332ddc0437c639d3b8d7c2f510f638b0d663
kaushalya <mkbkaushalya@gmail.com>
2022-04-23 10:58:35 +0530

    Add Power consumption and get  power consumption parts completed set db connections. server running

---
+++ ElectroGrid-EG-PowerCompany-Api/src/main/java/com/electrogrid/ElectroGrid_EG_PowerCompany_Api/PowerResource.java
@@ -0,0 +1,40 @@
+package com.electrogrid.ElectroGrid_EG_PowerCompany_Api;
+
+import java.util.List;
+
+import com.electrogrid.model.Power;
+import com.electrogrid.model.User;
+import com.electrogrid.repository.PowerRepository;
+import com.electrogrid.repository.UserRepository;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path ("power")
+
+
+public class PowerResource {
+
+	
+PowerRepository powerRepository = new PowerRepository();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Power> getPower() {
+		System.out.println("get power call");
+		
+		return powerRepository.getPowers();
+	}
+	
+	@POST
+	@Path("add-power")
+	public Power addNewPower(Power power) {
+		System.out.println(power);
+		powerRepository.addPower(power);
+		
+		return power;
+	}
+}
---
+++ ElectroGrid-EG-PowerCompany-Api/src/main/java/com/electrogrid/repository/PowerRepository.java
@@ -0,0 +1,86 @@
+package com.electrogrid.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+
+import com.electrogrid.dbcontext.DatabaseConnection;
+import com.electrogrid.dbcontext.IDBContext;
+import com.electrogrid.model.Power;
+import com.electrogrid.model.User;
+import com.sun.java_cup.internal.runtime.Scanner;
+import com.sun.java_cup.internal.runtime.Symbol;
+
+
+public class PowerRepository {
+	private Connection connection = null;
+	private IDBContext dbContext;
+	private Statement statement;
+	private ResultSet resultSet;
+	private static PreparedStatement preparedStatement = null;
+	
+	public PowerRepository() {
+		super();
+		this.dbContext = new DatabaseConnection();
+		this.connection = dbContext.getDatabaseConnection();
+		
+	}
+	
+	
+	
+	
+	
+	//get all powers acc from the database
+	public List<Power> getPowers(){
+		
+		List<Power> powers = new ArrayList<Power>();
+		String sql = "SELECT * FROM powers";
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sql);
+			while (resultSet.next()) {
+				Power power = new Power();
+				power.setId(resultSet.getString(1));
+				power.setPower_connection_type(resultSet.getString(2));
+				power.setPower_bill_id(resultSet.getString(3));
+				power.setPower_used_units(resultSet.getString(4));
+				power.setPower_unit_value(resultSet.getString(5));
+				power.setPower_pay_value(resultSet.getString(6));
+				power.setPower_pay_balance(resultSet.getString(7));
+				power.setPower_bill_value(resultSet.getString(7));
+				
+				powers.add(power);
+			}
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return powers;
+	}
+	
+	
+	
+	
+	
+	//add new users for the data base
+		public void addPower(Power power) {
+			String sql = "insert into power value (?,?,?,?,?,?,?,?)";
+			try {
+				PreparedStatement preparedStatement = connection.prepareStatement(sql);
+				//preparedStatement.setString(1,	user.getId());
+				preparedStatement.setString(2,power.getPower_connection_type());
+				preparedStatement.setString(3, power.getPower_bill_id());
+				preparedStatement.setString(4, power.getPower_used_units());
+				preparedStatement.setString(5, power.getPower_unit_value());
+				preparedStatement.setString(6, power.getPower_pay_value());
+				preparedStatement.setString(7, power.getPower_pay_balance());
+				preparedStatement.setString(8, power.getPower_bill_value());
+				
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+
+	
+}
