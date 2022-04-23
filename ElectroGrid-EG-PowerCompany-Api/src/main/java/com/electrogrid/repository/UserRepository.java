ba5c7cba170944a6740202c238c57b7ca7bb1503
Ranasinghe TKSA <sachin.ranasinghe1998.13@gmail.com>
2022-04-23 18:07:41 +0530

    user management function is done

--- ElectroGrid-EG-PowerCompany-Api/src/main/java/com/electrogrid/repository/UserRepository.java
+++ ElectroGrid-EG-PowerCompany-Api/src/main/java/com/electrogrid/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.electrogrid.repository;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import javax.swing.table.TableStringConverter;
+
 import java.sql.*;
 
 import com.electrogrid.dbcontext.DatabaseConnection;
@@ -34,8 +38,8 @@ public class UserRepository {
 			Statement statement = connection.createStatement();
 			ResultSet resultSet = statement.executeQuery(sql);
 			while (resultSet.next()) {
-				User user = new User();
-				user.setId(resultSet.getString(1));
+				User user = new User();		
+				user.setId(resultSet.getInt(1));
 				user.setUser_name(resultSet.getString(2));
 				user.setUser_nic(resultSet.getString(3));
 				user.setUser_address(resultSet.getString(4));
@@ -53,9 +57,37 @@ public class UserRepository {
 		return users;
 	}
 	
-	//get user by user id
-	public User getUserById(int id) {
-		return null;
+	// get one user from the database
+	public List<User> getUser(int id){
+		
+		List<User> users = new ArrayList<User>();
+		String sql = " SELECT * FROM `electro_grid`.`user` WHERE (`id` = "+id+");";
+		try {
+			
+			//PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			//preparedStatement.setInt(1, id);
+	
+			
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sql);
+			while (resultSet.next()) {
+				User user = new User();		
+				user.setId(resultSet.getInt(1));
+				user.setUser_name(resultSet.getString(2));
+				user.setUser_nic(resultSet.getString(3));
+				user.setUser_address(resultSet.getString(4));
+				user.setUser_zip_code(resultSet.getString(5));
+				user.setUser_contact_number(resultSet.getString(6));
+				user.setUser_email(resultSet.getString(7));
+				
+				users.add(user);
+			}
+			//preparedStatement.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return users;
 	}
 	
 	//add new users for the data base
@@ -63,8 +95,9 @@ public class UserRepository {
 		String sql = "insert into user value (?,?,?,?,?,?,?)";
 		try {
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setString(1,	user.getId());
-			preparedStatement.setString(2,user.getUser_name());
+						
+			preparedStatement.setInt(1, user.getId());
+			preparedStatement.setString(2, user.getUser_name());
 			preparedStatement.setString(3, user.getUser_nic());
 			preparedStatement.setString(4, user.getUser_address());
 			preparedStatement.setString(5, user.getUser_zip_code());
@@ -78,15 +111,59 @@ public class UserRepository {
 		}
 	}
 
-	// update user of the database
-	public void update(User user) {
-		// TODO Auto-generated method stub
+	// update user details
+	public String updateUser(User user) {
 		
+		String sql = "UPDATE `electro_grid`.`user` SET `name` = ?, `nic` = ?, `address` = ?, `zipcode` = ?, `contact number` =? , `email` = ? WHERE `id` = ?";
+		String response = null;
+		
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(sql); 
+			preparedStatement.setString(1, user.getUser_name());
+			preparedStatement.setString(2, user.getUser_nic());
+			preparedStatement.setString(3, user.getUser_address());
+			preparedStatement.setString(4, user.getUser_zip_code());
+			preparedStatement.setString(5, user.getUser_contact_number());
+			preparedStatement.setString(6, user.getUser_email());
+			preparedStatement.setInt(7, user.getId());
+			
+			preparedStatement.executeUpdate();
+			
+			response = "successfuly update\n"
+					+"ID: "+ user.getId()+"\n"
+					+"User_name: "+ user.getUser_name()+"\n"
+					+"getUser_nic: "+ user.getUser_nic()+"\n"
+					+"User_address: "+ user.getUser_address()+"\n"
+					+"User_zip_code: "+ user.getUser_zip_code()+"\n" 
+					+"User_contact_number: "+ user.getUser_contact_number()+"\n" 
+					+"getUser_email: "+ user.getUser_email() +"\n";
+			
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return response;
 	}
-
 	
+	// delete user from the database
+	public String detele(int id) {
+		
+		String sql = "DELETE FROM `electro_grid`.`user` WHERE (`id` = ?);";
+		String response = null;
+		
+		try {
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setInt(1, id);
+			statement.executeUpdate();
+			
+			response = "successfuly Deleted"; 
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return response;
+	}
 	
-	
-
 	
 }
